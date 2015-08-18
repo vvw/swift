010fd7eac38764527f6454ba6e06bec5a27845ef
vvw <siluplsy@gmail.com>
2015-08-18 20:54:38 +0800

    trivial

---
+++ safari/testExt.safariextension/Injected.js
@@ -0,0 +1,16 @@
+var theBody = document.body;
+// create a para and insert it at the top of the body
+var element = document.createElement("p");
+element.id = "status";
+element.style.cssText = "float:right; color:red";
+element.textContent = "Waiting...";
+theBody.insertBefore(element, theBody.firstChild);
+
+function replyToMessage(aMessageEvent) {
+	if (aMessageEvent.name === "hey") {
+		document.getElementById("status").textContent = "Message received.";
+		safari.self.tab.dispatchMessage("gotIt", "Message acknowledged.");
+	}
+}
+// register for message events
+safari.self.addEventListener("message", replyToMessage, false);
