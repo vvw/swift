8df26cfb4b4ca8a03afd3b0608806aa458a12988
vvw <siluplsy@gmail.com>
2015-08-20 17:54:48 +0800

    trivial

--- safari/testExt.safariextension/serv.js
+++ safari/testExt.safariextension/serv.js
@@ -1,7 +1,7 @@
 var port = 8888;
 var http = require('http');
 http.createServer(function (req, res) {
-  var data = "<!DOCTYPE html><html><head><title>Hello World</title></head><body>Hello World!</body></html>";
+  var data = "<!DOCTYPE html><html><head><title>Hello World</title></head><body>Hello World! respond by nodejs server.</body></html>";
   res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
   res.write(data);
   res.end();
